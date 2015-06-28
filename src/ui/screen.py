55a05463e948d2abf584d73b481e428b46304126
Fernando <fgmoralesc@gmail.com>
2015-06-28 03:37:50 -0300

    Vars on screen private

--- src/ui/screen.py
+++ src/ui/screen.py
@@ -6,10 +6,10 @@ import pygame
 class Screen:
 
     def __init__(self, title, screen_width, screen_height):
-        self.screen_width = screen_width
-        self.screen_height = screen_height
-        self.title = title
+        self.__screen_width = screen_width
+        self.__screen_height = screen_height
+        self.__title = title
 
     def draw(self):
-        pygame.display.set_mode((self.screen_width, self.screen_height))
-        pygame.display.set_caption(self.title)
+        pygame.display.set_mode((self.__screen_width, self.__screen_height))
+        pygame.display.set_caption(self.__title)
